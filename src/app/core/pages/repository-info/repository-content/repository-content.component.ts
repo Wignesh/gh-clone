99e4f486753ad892815a5f07f8c9212d0bf96691
droidLight <sibinehru@gmail.com>
2021-06-23 11:15:38 +0530

    added repository-content component

---
+++ src/app/core/pages/repository-info/repository-content/repository-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-content',
+  templateUrl: './repository-content.component.html',
+  styleUrls: ['./repository-content.component.scss']
+})
+export class RepositoryContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
