a99b307d7c1608659d407e0b313b4a57227880c5
droidLight <sibinehru@gmail.com>
2021-06-24 23:38:51 +0530

    added code-view component

---
+++ src/app/core/pages/repository-info/code-view/code-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-view',
+  templateUrl: './code-view.component.html',
+  styleUrls: ['./code-view.component.scss']
+})
+export class CodeViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
