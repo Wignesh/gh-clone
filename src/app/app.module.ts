4e291e32892fb95685095396bd7cc33240192ceb
VIGNESH J <vignesh@programmer.net>
2021-06-24 21:03:16 +0530

    Added repo search
    Added:
    
    Repo Search
    Load more repo
    
    Fixed:
    
    Device width issue, support upto tablet
    
    Signed-off-by: VIGNESH J <vignesh@programmer.net>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginPage } from './core/pages/login/login.page';
 import { DashboardPage } from './core/pages/dashboard/dashboard.page';
@@ -50,7 +51,7 @@ import { DashboardNewsFeedSidebarRightComponent } from './core/pages/dashboard/d
     DashboardNewsFeedComponent,
     DashboardNewsFeedSidebarRightComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/pages/dashboard/dashboard-sidebar-left/dashboard-sidebar-left.component.ts
+++ src/app/core/pages/dashboard/dashboard-sidebar-left/dashboard-sidebar-left.component.ts
@@ -7,8 +7,119 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardSidebarLeftComponent implements OnInit {
   isSidebarLoading: boolean = true;
+  repos: any = [];
+  _repos: any = [];
+  isLoadingMore: boolean = false;
+  searchTerm: string = '';
 
-  constructor() {}
+  constructor() {
+    this._repos = [
+      {
+        name: 'Wignesh/gh-clone',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/coda-training',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/farmiest',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/Wignesh',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/angular-todo',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/mailway-org',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/Java',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/books',
+        isPrivate: false,
+      },
+      {
+        name: 'mailway-app/mailway',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/bd-env',
+        isPrivate: false,
+      },
+      {
+        name: 'okteto/okteto',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/docker',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/Java-Design-Patter',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/Linkedin',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/okteto-sync',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/hpl',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/rn-stocks',
+        isPrivate: true,
+      },
+      {
+        name: 'Wignesh/js-image-compressor',
+        isPrivate: false,
+      },
+      {
+        name: 'Wignesh/geny-auto',
+        isPrivate: true,
+      },
+      {
+        name: 'e2necc',
+        isPrivate: false,
+      },
+      {
+        name: 'flask-todo',
+        isPrivate: false,
+      },
+    ];
+    this.repos = this._repos.slice(0, 7);
+  }
+
+  showMoreRepos() {
+    this.isLoadingMore = true;
+    if (this.searchTerm == '') {
+      setTimeout(() => {
+        this.isLoadingMore = false;
+        this.repos = this._repos;
+      }, 1000);
+    }
+  }
+
+  searchRepo() {
+    if (this.searchTerm == '') {
+      this.repos = this._repos.slice(0, 7);
+    }
+    this.repos = this._repos.filter((repo: any) => {
+      return repo.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+    });
+  }
 
   ngOnInit(): void {
     setTimeout(() => {
